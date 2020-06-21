14654c130b238aa133e184bc5ce5169865fe1587
Yvan Burrie <yvan.burrie@hotmail.com>
2020-06-21 15:52:54 +1000

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -12,11 +12,17 @@ using Ensemble::Registry;
 int main()
 {
     // Initialise:
-    auto r = new Registry("Software\\Some\\Stuff");
+    auto r = new Registry("Software\\Sample");
     bool b;
 
+    // Sample string:
+    char c[200];
+    strcpy(c, "some test");
+    DWORD l = strlen((const char*)c);
+    b = r->RegSetAscii(false, "sample-ascii", LPBYTE(c), l + 1);
+
     // Sample integer:
-    long i = 101;
+    DWORD i = 101;
     b = r->RegSetInt(false, "sample-integer", i);
     printf("Sample integer (%ld) added? %s", i, b ? "Y" : "N");
     i = r->RegGetInt(false, "sample-integer");
